c98259c2f302286d86408344eaa7f550366e4cb2
dhanushrajendra <dhanush.gr@gmail.com>
2023-08-15 17:22:46 +0530

    changes

--- src/views/website-builder/websitebuilderheader.tsx
+++ src/views/website-builder/websitebuilderheader.tsx
@@ -1,11 +1,24 @@
 const WebsiteBuilderHeader = () => {
-    return (
-        <div className="website-builder-header">
-          <div className="website-builder-header-item">
-                header content
-          </div>
+  return (
+    <div className="website-builder-header">
+      <div className="website-builder-header-item">
+        <div className="product-name">Product Name</div>
+        <div className="actions">
+          <p>Undo</p>
+          <p>Redo</p>
         </div>
-      );
-}
+        <div className="views">
+          <p>Desktop</p>
+          <p>Mobile</p>
+        </div>
+        <div className="user-actions">
+          <p>Preview</p>
+          <button className="save">Save</button>
+          <button className="publish">Publish</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default WebsiteBuilderHeader;
