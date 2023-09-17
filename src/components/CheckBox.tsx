dc5aca0e93d56ad3eeb755e7e4fc9bd563f1df58
krazy6i <25668626+krazys@users.noreply.github.com>
2023-09-18 01:23:44 +0530

    Smart to Website Builder flow logic added

--- src/components/CheckBox.tsx
+++ src/components/CheckBox.tsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface Props {
   checkBoxName: string;
 }
 
 const CheckBox: React.FC<Props> = ({ checkBoxName }) => {
+  const [isChecked, setIsChecked] = useState<boolean>(false);
   return (
     <div className="checkbox">
-      <input type="checkbox" /> {checkBoxName}
+      <input
+        type="checkbox"
+        checked={isChecked}
+        onChange={() => setIsChecked((prev) => !prev)}
+      />{" "}
+      {checkBoxName}
     </div>
   );
 };
--- src/views/smart-builder/get-started.tsx
+++ src/views/smart-builder/get-started.tsx
@@ -1,20 +1,28 @@
 import ComponentHeader from "../../components/componentHeader";
 import ComponentSubHeader from "../../components/componentSubheader";
+import { useNavigate } from "react-router-dom";
 
 const GetStarted = () => {
+  const navigate = useNavigate();
+  const handleNavigate = () => {
+    navigate("/dashboard/smart-builder/question-one");
+  };
   return (
     <div className="smart-builder">
       <div className="smart-builder-item">
-      <ComponentHeader heading={"Product Name"} />
-      <ComponentSubHeader
+        <ComponentHeader heading={"Product Name"} />
+        <ComponentSubHeader
           subheading={"Help our experts build your page"}
           specialWord=""
         />
         <p>
-            Answer a set of questions based on marketing strategy & user experience.
+          Answer a set of questions based on marketing strategy & user
+          experience.
         </p>
-        <button className="smart-builder"> Use Smart Builder </button>
-        
+        <button className="smart-builder" onClick={handleNavigate}>
+          {" "}
+          Use Smart Builder{" "}
+        </button>
       </div>
     </div>
   );
