60b6520efdb7e12bfd0fcaee25926ea71b9be11e
Ashish Rao D S <ashishraods@SMGCC-MAC014.local>
2023-10-28 19:32:47 +0530

    fixing the build issues

--- src/app/(authenticated-routes)/projects/page.tsx
+++ src/app/(authenticated-routes)/projects/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 import ItemListing from "@/components/Project/ItemListing";
-import List from "@/components/Project/List";
 import NewItem from "@/components/Project/NewItem";
 import NewItemPopup from "@/components/Project/NewItemPopup";
 import RecentSection from "@/components/Project/RecentSection";
@@ -9,6 +8,8 @@ import { createProjectForOrganization, deleteProjectByProjectId, getAllProjectsB
 import { getUserBySub } from "@/services/UserService";
 import { Auth } from "aws-amplify";
 import { CellContext } from "@tanstack/react-table";
+import { ProjectTableData } from "@/types/project";
+
 export interface NewProjectPayload {
   inputOneData: string;
   inputTwoData: string;
@@ -22,7 +23,7 @@ export type Project = {
 
 export default function Page() {
   const [isAddNewProjectModalOpen, setIsAddNewProjectModalOpen] = useState(false)
-  const [tableData, setTableData] = useState([])
+  const [tableData, setTableData] = useState<ProjectTableData[]>([])
 
   const columnHeaders = [
     'Name',
