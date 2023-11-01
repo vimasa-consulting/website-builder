2c8c03097347644944cb7385bde9f3c3b12e9b4e
Gorbin Philip <41995531+gorbinphilip@users.noreply.github.com>
2023-11-02 00:33:48 +0530

    Revert "File & Project Settings Popup Added"

--- src/components/Identity/Authenticator.tsx
+++ src/components/Identity/Authenticator.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client'
 
 import React, { useEffect } from "react";
 import { Amplify, Auth, Hub } from "aws-amplify";
@@ -6,39 +6,37 @@ import {
   Authenticator as AmplifyAuthenticator,
   CheckboxField,
   TextField,
-  useAuthenticator as useAmplifyAuthenticator,
+  useAuthenticator as useAmplifyAuthenticator
 } from "@aws-amplify/ui-react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
-import "@aws-amplify/ui-react/styles.css";
-import { ThemeProvider, Theme } from "@aws-amplify/ui-react";
-import { Inter } from "next/font/google";
+import '@aws-amplify/ui-react/styles.css';
+import { ThemeProvider, Theme } from '@aws-amplify/ui-react';
+import { Inter } from 'next/font/google'
 
-import styles from "@/styles/authenticator.module.css";
-import awsExports from "../../../aws-exports";
+import styles from '@/styles/authenticator.module.css';
+import awsExports from "@/aws-exports";
 import { ROUTES } from "@/services/NavigationService";
 import { postSignInActions } from "@/services/AuthService";
 
-const font = Inter({ subsets: ["latin"] });
+const font = Inter({ subsets: ['latin'] })
 Amplify.configure({ ...awsExports, ssr: true });
 
 type AuthenticatorProps = {
-  initialState: "signUp" | "signIn";
-  children: React.ReactNode;
-};
+  initialState: 'signUp' | 'signIn',
+  children: React.ReactNode
+}
 
-function AmplifyAuthenticatorWrapper(
-  authenticatorProps: AuthenticatorProps
-): React.JSX.Element {
+function AmplifyAuthenticatorWrapper(authenticatorProps: AuthenticatorProps): React.JSX.Element {
   const router = useRouter();
 
   // TODO: use authStatus from context instead of following listeners
 
   // Register hub listener for post sign in actions
   useEffect(() => {
-    const authListener = Hub.listen("auth", (data) => {
+    const authListener = Hub.listen('auth', (data) => {
       const { payload } = data;
-      if (payload?.event === "signIn") {
+      if (payload?.event === 'signIn') {
         // TODO: route to sign in redirection source
         postSignInActions().then(() => {
           router.replace(ROUTES.PROJECTS);
@@ -47,21 +45,19 @@ function AmplifyAuthenticatorWrapper(
     });
     return () => {
       authListener();
-    };
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   // Handle if already signed in
   useEffect(() => {
-    Auth.currentAuthenticatedUser()
-      .then(() => {
-        postSignInActions().then(() => {
-          router.replace(ROUTES.PROJECTS);
-        });
-      })
-      .catch(() => {
-        // ignore
+    Auth.currentAuthenticatedUser().then(() => {
+      postSignInActions().then(() => {
+        router.replace(ROUTES.PROJECTS);
       });
+    }).catch(() => {
+      // ignore
+    });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -72,38 +68,27 @@ function AmplifyAuthenticatorWrapper(
           <div className="flex items-center justify-center flex-col pt-6">
             <h2 className="text-3xl">Create an account</h2>
             <div className="text-base mt-4">
-              Already have an account?{" "}
-              <Link href={ROUTES.SIGN_IN} className={styles.actionLink}>
-                Sign in
-              </Link>
+              Already have an account? <Link href={ROUTES.SIGN_IN} className={styles.actionLink}>Sign in</Link>
             </div>
           </div>
         );
       },
       Footer() {
-        return <></>;
+        return (<></>);
       },
       FormFields() {
         const { validationErrors } = useAmplifyAuthenticator();
         return (
           <>
             <div className="md:flex gap-4">
-              <TextField
-                name="given_name"
-                label="First name"
-                className="md:w-1/2"
-              />
-              <TextField
-                name="family_name"
-                label="Last name"
-                className="md:w-1/2"
-              />
+              <TextField name="given_name" label="First name" className="md:w-1/2" />
+              <TextField name="family_name" label="Last name" className="md:w-1/2" />
             </div>
             <AmplifyAuthenticator.SignUp.FormFields />
             {/* Append & require Terms & Conditions field to sign up  */}
             <CheckboxField
-              marginTop={"2rem"}
-              marginBottom={"0.5rem"}
+              marginTop={'2rem'}
+              marginBottom={'0.5rem'}
               errorMessage={validationErrors.acknowledgement as string}
               hasError={!!validationErrors.acknowledgement}
               name="acknowledgement"
@@ -137,10 +122,7 @@ function AmplifyAuthenticatorWrapper(
           <div className="flex items-center justify-center flex-col pt-6">
             <h2 className="text-3xl">Welcome back!</h2>
             <div className="text-base mt-4">
-              Don&apos;t have an account?{" "}
-              <Link href={ROUTES.SIGN_UP} className={styles.actionLink}>
-                Sign up
-              </Link>
+              Don&apos;t have an account? <Link href={ROUTES.SIGN_UP} className={styles.actionLink}>Sign up</Link>
             </div>
           </div>
         );
@@ -149,7 +131,10 @@ function AmplifyAuthenticatorWrapper(
         const { toResetPassword } = useAmplifyAuthenticator();
         return (
           <div className="flex flex-col items-center pb-4">
-            <button onClick={toResetPassword} className={styles.actionLink}>
+            <button
+              onClick={toResetPassword}
+              className={styles.actionLink}
+            >
               Forgot password?
             </button>
           </div>
@@ -193,22 +178,21 @@ function AmplifyAuthenticatorWrapper(
   const formFields = {
     signUp: {
       email: {
-        placeholder: "example@company.com",
-      },
+        placeholder: 'example@company.com'
+      }
     },
     signIn: {
       username: {
-        placeholder: "example@company.com",
-      },
-    },
-  };
+        placeholder: 'example@company.com'
+      }
+    }
+  }
 
   const services = {
     async validateCustomSignUp(formData: any, touchData: any) {
       if (!formData.acknowledgement && touchData.acknowledgement) {
         return {
-          acknowledgement:
-            "You must agree to the Terms of Use and Privacy Policy.",
+          acknowledgement: 'You must agree to the Terms of Use and Privacy Policy.',
         };
       }
     },
@@ -228,30 +212,28 @@ function AmplifyAuthenticatorWrapper(
   );
 }
 
-export default function Authenticator(
-  authenticatorProps: AuthenticatorProps
-): React.JSX.Element {
+export default function Authenticator(authenticatorProps: AuthenticatorProps): React.JSX.Element {
   const theme: Theme = {
-    name: "amplify-theme",
+    name: 'amplify-theme',
     tokens: {
       colors: {
         brand: {
           primary: {
-            "10": "{colors.purple.10.value}",
-            "20": "{colors.purple.20.value}",
-            "40": "{colors.purple.40.value}",
-            "60": "{colors.purple.60.value}",
-            "80": "{colors.purple.80.value}",
-            "90": "{colors.purple.90.value}",
-            "100": "{colors.purple.100.value}",
-          },
-        },
+            "10": '{colors.purple.10.value}',
+            "20": '{colors.purple.20.value}',
+            "40": '{colors.purple.40.value}',
+            "60": '{colors.purple.60.value}',
+            "80": '{colors.purple.80.value}',
+            "90": '{colors.purple.90.value}',
+            "100": '{colors.purple.100.value}',
+          }
+        }
       },
       fonts: {
         default: {
-          variable: { value: font.style.fontFamily },
-        },
-      },
+          variable: { value: font.style.fontFamily }
+        }
+      }
     },
   };
 
@@ -259,13 +241,11 @@ export default function Authenticator(
     <div>
       <ThemeProvider theme={theme}>
         <AmplifyAuthenticator.Provider>
-          <AmplifyAuthenticatorWrapper
-            initialState={authenticatorProps.initialState}
-          >
+          <AmplifyAuthenticatorWrapper initialState={authenticatorProps.initialState}>
             {authenticatorProps.children}
           </AmplifyAuthenticatorWrapper>
         </AmplifyAuthenticator.Provider>
       </ThemeProvider>
     </div>
-  );
+  )
 }
--- src/components/Project/RecentSection.tsx
+++ src/components/Project/RecentSection.tsx
@@ -4,13 +4,13 @@ import Item from "./RecentItem";
 export default function RecentSection({
   recentItems,
   itemType
-}) {
+}: any) {
 
   return (
     <>
       <h3 className="text-xl">Recently Edited</h3>
       <div className="flex flex-wrap mt-5 gap-3">
-        {recentItems?.length ? recentItems.map((project) => <Item itemType={itemType} key={project._id} project={project} />) :
+        {recentItems?.length ? recentItems.map((project: any) => <Item itemType={itemType} key={project._id} project={project} />) :
         Array.from({length: 2}).map((_, index) => <CardItemSkeleton key={index} />)
         }
       </div>
