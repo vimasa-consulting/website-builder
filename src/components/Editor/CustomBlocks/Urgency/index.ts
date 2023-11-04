94dcabd240cbbd47b23e32f292914610dd7e0ffd
niranjanbala <niranjan.bala@gmail.com>
2023-11-05 01:30:45 +0530

    added fomo2 component

--- src/components/Editor/CustomBlocks/Urgency/index.ts
+++ src/components/Editor/CustomBlocks/Urgency/index.ts
@@ -4,7 +4,17 @@ fomo1Block,
 fomo1Component
 } from "./Blocks/fomo1";
 
+import {
+  fomo2Block,
+  fomo2Component
+  } from "./Blocks/fomo2";
+  
+
 export default function initUrgency(editor: Editor) {
   fomo1Block(editor);
   fomo1Component(editor);
+
+  fomo2Block(editor);
+  fomo2Component(editor);
+
 }
