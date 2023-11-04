c5e82b30ee3be6f6d0b5b0901e37944a71f02e79
Ashish Rao D S <ashishraods@SMGCC-MAC014.local>
2023-11-04 14:19:48 +0530

    feat: template for Logic category - How it works 1

--- src/components/Editor/CustomBlocks/initialization.ts
+++ src/components/Editor/CustomBlocks/initialization.ts
@@ -1,8 +1,10 @@
 import { Editor } from "grapesjs";
 import initFirstImpression from "./FirstImpression/index";
 import initTrust from "./Trust";
+import initLogic from "./Logic";
 
 export default function init(editor: Editor) {
   initFirstImpression(editor);
   initTrust(editor)
+  initLogic(editor)
 }
