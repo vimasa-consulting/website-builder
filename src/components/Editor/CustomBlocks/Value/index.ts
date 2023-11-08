10a28706984f987303a154dee15eb15736e49dbb
Devesh Verma <deveshverma619@gmail.com>
2023-11-08 23:21:22 +0530

    added value components - multi product offer 1,2,3

--- src/components/Editor/CustomBlocks/Value/index.ts
+++ src/components/Editor/CustomBlocks/Value/index.ts
@@ -3,12 +3,36 @@ import {
   shopByOfferValue1Block,
   shopByOfferValue1Component,
 } from "./Blocks/shopByOfferVal1";
-import { shopByOfferValue2Block } from "./Blocks/shopByOfferVal2";
+import {
+  shopByOfferValue2Block,
+  shopByOfferValue2Component,
+} from "./Blocks/shopByOfferVal2";
+import {
+  multiProductOffers1Block,
+  multiProductOffers1Component,
+} from "./Blocks/multiProductOffer1";
+import {
+  multiProductOffers2Block,
+  multiProductOffers2Component,
+} from "./Blocks/multiProductOffer2";
+import {
+  multiProductOffers3Block,
+  multiProductOffers3Component,
+} from "./Blocks/multiProductOffer3";
 
 export default function initValue(editor: Editor) {
   shopByOfferValue1Block(editor);
   shopByOfferValue1Component(editor);
 
   shopByOfferValue2Block(editor);
-  shopByOfferValue1Component(editor);
+  shopByOfferValue2Component(editor);
+
+  multiProductOffers1Block(editor);
+  multiProductOffers1Component(editor);
+
+  multiProductOffers2Block(editor);
+  multiProductOffers2Component(editor);
+
+  multiProductOffers3Block(editor);
+  multiProductOffers3Component(editor);
 }
