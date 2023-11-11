49cf7b47e77e92032da6e69cf7095ad79060b9fc
Shashi Shekhar <shashishekhar@Shashis-MacBook-Air.local>
2023-11-11 15:32:05 +0530

    all timeline desktop finished

---
+++ src/components/Editor/CustomBlocks/Logic/Blocks/timeline1.ts
@@ -0,0 +1,243 @@
+import { Editor } from "grapesjs";
+
+const timeline1Block = (editor: Editor) => {
+  const options = {
+    id: "timeline1",
+    label: "Timeline 1",
+    block: {},
+    props: {},
+    style: "",
+    category: "Logic",
+    classPrefix: "timeline1",
+    styleAdditional: "",
+    media: `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-sign-intersection-fill" viewBox="0 0 16 16">
+      <path d="M9.05.435c-.58-.58-1.52-.58-2.1 0L.436 6.95c-.58.58-.58 1.519 0 2.098l6.516 6.516c.58.58 1.519.58 2.098 0l6.516-6.516c.58-.58.58-1.519 0-2.098L9.05.435ZM7.25 4h1.5v3.25H12v1.5H8.75V12h-1.5V8.75H4v-1.5h3.25V4Z"/>
+    </svg>`,
+  };
+
+  editor.Blocks.add(options.id, {
+    media: options.media,
+    label: options.label,
+    category: options.category,
+    content: { type: options.id },
+    ...options.block,
+  });
+};
+
+const timeline1Component = (editor: Editor) => {
+  const options = {
+    id: "timeline1",
+    label: "Timeline 1",
+    block: {},
+    props: {},
+    style: "",
+    category: "Logic",
+    classPrefix: "timeline1",
+    styleAdditional: "",
+    media: `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-sign-intersection-fill" viewBox="0 0 16 16">
+    <path d="M9.05.435c-.58-.58-1.52-.58-2.1 0L.436 6.95c-.58.58-.58 1.519 0 2.098l6.516 6.516c.58.58 1.519.58 2.098 0l6.516-6.516c.58-.58.58-1.519 0-2.098L9.05.435ZM7.25 4h1.5v3.25H12v1.5H8.75V12h-1.5V8.75H4v-1.5h3.25V4Z"/>
+    </svg>`,
+  };
+  const { classPrefix } = options;
+
+  editor.Components.addType(options.id, {
+    model: {
+      defaults: {
+        classes: [classPrefix],
+        traits: [],
+        components: `
+<section class="${classPrefix}-section">
+  <div class="${classPrefix}-wrapper">
+      <div class="${classPrefix}-textWrapper">
+        <p>Catchy headline about your
+        Product’s Measurable results</p>
+      </div>
+      <div class="${classPrefix}-lineContainer">
+        <svg xmlns="http://www.w3.org/2000/svg" width="1220" height="2" viewBox="0 0 1220 2" fill="none">
+          <path d="M0 1H1220" stroke="#999999" stroke-width="2"/>
+        </svg>
+      </div>
+      <div class="${classPrefix}-container">
+        <div class="${classPrefix}-childContainer">
+          <p class="${classPrefix}-imgWrapper">
+            <img loading="lazy" src="/editor/component-icons/tv.png" class="${classPrefix}-imgCover" />
+          </p>
+          <p class="${classPrefix}-circleWrapper">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+              <circle cx="10" cy="10" r="10" fill="#999999"/>
+            </svg>
+          </p>
+          <div class="${classPrefix}-cardContainer">
+            <p class="${classPrefix}-title">Timeline 1</p>
+            <p class="${classPrefix}-desc">Description of the expected result at this stage</p>
+          </div>
+        </div>
+        <div class="${classPrefix}-childContainer">
+          <p class="${classPrefix}-imgWrapper">
+            <img loading="lazy" src="/editor/component-icons/charging-battery.png" class="${classPrefix}-imgCover" />
+          </p>
+          <p class="${classPrefix}-circleWrapper">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+              <circle cx="10" cy="10" r="10" fill="#999999"/>
+            </svg>
+          </p>
+          <div class="${classPrefix}-cardContainer">
+            <p class="${classPrefix}-title">Timeline 2</p>
+            <p class="${classPrefix}-desc">Description of the expected result at this stage</p>
+          </div>
+        </div>
+        <div class="${classPrefix}-childContainer">
+          <p class="${classPrefix}-imgWrapper">
+            <img loading="lazy" src="/editor/component-icons/cloud-big.png" class="${classPrefix}-imgCover" />
+          </p>
+          <p class="${classPrefix}-circleWrapper">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+              <circle cx="10" cy="10" r="10" fill="#999999"/>
+            </svg>
+          </p>
+          <div class="${classPrefix}-cardContainer">
+            <p class="${classPrefix}-title">Timeline 3</p>
+            <p class="${classPrefix}-desc">Description of the expected result at this stage</p>
+          </div>
+        </div>
+        <div class="${classPrefix}-childContainer">
+          <p class="${classPrefix}-imgWrapper">
+            <img loading="lazy" src="/editor/component-icons/chart-2.png" class="${classPrefix}-imgCover" />
+          </p>
+          <p class="${classPrefix}-circleWrapper">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+              <circle cx="10" cy="10" r="10" fill="#999999"/>
+            </svg>
+          </p>
+          <div class="${classPrefix}-cardContainer">
+            <p class="${classPrefix}-title">Timeline 4</p>
+            <p class="${classPrefix}-desc">Description of the expected result at this stage</p>
+          </div>
+        </div>
+        <div class="${classPrefix}-childContainer">
+          <p class="${classPrefix}-imgWrapper">
+            <img loading="lazy" src="/editor/component-icons/hand-shake.png" class="${classPrefix}-imgCover" />
+          </p>
+          <p class="${classPrefix}-circleWrapper">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+              <circle cx="10" cy="10" r="10" fill="#999999"/>
+            </svg>
+          </p>
+          <div class="${classPrefix}-cardContainer">
+            <p class="${classPrefix}-title">Timeline 5</p>
+            <p class="${classPrefix}-desc">Description of the expected result at this stage</p>
+          </div>
+        </div>
+      </div>
+  </div>
+</section>
+        `,
+        styles: `
+.${classPrefix}-h1 {
+  display: block;
+  font-size: 40px;
+  font-weight: 600;
+}
+
+.${classPrefix}-section {
+  background-color: #fff;
+  display: flex;
+  flex-direction: column;
+  padding: 80px;
+}
+
+.${classPrefix}-wrapper {
+  position: relative;
+}
+
+.${classPrefix}-lineContainer {
+  position: absolute;
+  top: 290px;
+  left: 14px;
+}
+
+.${classPrefix}-textWrapper {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 20px;
+}
+
+.${classPrefix}-textWrapper > p {
+  width: 564px;
+  color: #000;
+  text-align: center;
+  font-family: Inter;
+  font-size: 40px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+}
+
+.${classPrefix}-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin-top: 24px;
+}
+
+.${classPrefix}-childContainer {
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  justify-content: center;
+  text-align: center;
+  align-items: center;
+}
+
+.${classPrefix}-cardContainer {
+  border-radius: 15px;
+  border: 1px solid #ABABAB;
+  width: 187px;
+  height: 153px;
+  padding: 18px;
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+}
+
+.${classPrefix}-circleWrapper {
+  margin-bottom: 20px;
+}
+
+.${classPrefix}-title {
+  margin-bottom: 0px;
+  color: #000;
+  font-family: Inter;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+}
+
+.${classPrefix}-desc {
+  margin-bottom: 10px;
+  color: #000;
+  font-family: Inter;
+  font-size: 15px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 140%; 
+}
+
+.${classPrefix}-imgWrapper {
+  margin-bottom: 7px;
+}
+
+@media (max-width: 425px) {
+  .${classPrefix}-section {
+    padding: 20px;
+  }
+}
+
+      `,
+      },
+    },
+  });
+};
+
+export { timeline1Block, timeline1Component };
--- src/components/Editor/CustomBlocks/Logic/index.ts
+++ src/components/Editor/CustomBlocks/Logic/index.ts
@@ -63,9 +63,18 @@ import {
   productData3Block,
   productData3Component
 } from "./Blocks/productData3";
+import { timeline2Block, timeline2Component } from "./Blocks/timeline2";
+import { timeline1Block, timeline1Component } from "./Blocks/timeline1";
+import { timeline3Block, timeline3Component } from "./Blocks/timeline3";
 
 export default function initLogic(editor: Editor) {
   // Before After 1
+  timeline1Block(editor);
+  timeline1Component(editor)
+  timeline2Block(editor);
+  timeline2Component(editor);
+  timeline3Block(editor);
+  timeline3Component(editor);
   howItWorks1Block(editor);
   howItWorks1Component(editor);
   howItWorks2Block(editor);
