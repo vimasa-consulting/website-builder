2a73a4fe6f32364e080f7937c64fe8409b0c3db5
Devesh Verma <deveshverma619@gmail.com>
2023-11-20 00:00:29 +0530

    completed brandstrory 2,3,4

--- src/components/Editor/CustomBlocks/BrandConnect/Blocks/brandStory2.ts
+++ src/components/Editor/CustomBlocks/BrandConnect/Blocks/brandStory2.ts
@@ -32,50 +32,50 @@ const brandStory2Component = (editor: Editor) => {
         traits: [],
         components: `
            <section class="${classPrefix}-section">
-              <div class="${classPrefix}-left-panel">
-               <h5 class="${classPrefix}-left-panel-heading">Compelling Header for Your Brand Story</h5> 
-              <div class="${classPrefix}-left-panel-img">
+            <div class="${classPrefix}-container">
+              <div class="${classPrefix}-left-container">
+                <h5 class="${classPrefix}-left-panel-heading">Compelling Header for Your Brand Story</h5>
+                <p class="${classPrefix}-left-panel-info">Craft your brand story in a way that sounds authentic, evokes emotions in your users, and
+                  is consistent across channels. Include the origin of the brand, its purpose & the values. Be very specific and
+                  simple with your brand message.</p>
               </div>
-               <p class="${classPrefix}-left-panel-info">Craft your brand story in a way that sounds authentic, evokes emotions in your users, and is consistent across channels. Include the origin of the brand, its purpose & the values. Be very specific and simple with your brand message.</p>
+              <div class="${classPrefix}-right-container">
+                <video class="${classPrefix}-video" controls>
+                  <source src="" type="video/mp4">
+                  Your browser does not support the video tag.
+                </video>
               </div>
-              <div class="${classPrefix}-panel">
-             </div>
-           </section>
-                    `,
+            </div>
+          </section>
+        `,
         styles: `
         .${classPrefix}-section {
-          padding: 84px 161px 83px 162px;
-          justify-content: center;
-          align-items: center;
-          justify-content: center;
-          align-items: center;
-          background-color: white;
+          max-height: 762;
+          max-width: 1440px;
+          padding: 84px 162px;
           display: flex;
-          max-height: 700px;
-        }
-        
-        .${classPrefix}-left-panel {
+          align-items: center;
+          align-items: center;
+          justify-content: center;
+          justify-content: center;
+          background-color: white;
+          }
+
+          .${classPrefix}-container {
+          max-width: 1117px;
+          max-height: 595px;
+          display: flex;
+          }
+
+          .${classPrefix}-left-container {
           display: flex;
           justify-content: center;
           align-items: center;
           flex-direction: column;
           flex: 1;
-        }
+          }
 
-        .${classPrefix}-left-panel-img {
-          display: none;
-          aspect-ratio: 1;
-          object-fit: contain;
-          object-position: center;
-          width: 90px;
-          mix-blend-mode: multiply;
-          overflow: hidden;
-          align-self: center;
-          max-width: 100%;
-          border-radius: 50%;
-        }
-
-        .${classPrefix}-left-panel-heading {
+          .${classPrefix}-left-panel-heading {
           color: #000;
           font-family: Inter;
           font-size: 40px;
@@ -83,31 +83,38 @@ const brandStory2Component = (editor: Editor) => {
           font-weight: 600;
           line-height: normal;
           margin: 0;
-        }
+          }
 
-        .${classPrefix}-left-panel-info {
+          .${classPrefix}-left-panel-info {
           color: #000;
           font-family: Inter;
           font-size: 15px;
           font-style: normal;
           font-weight: 400;
           line-height: 140%; /* 21px */
-          padding-top: 26px; 
-        }
+          padding-top: 26px;
+          margin: 0px;
+          }
 
-        .${classPrefix}-panel {
+          .${classPrefix}-right-container {
           border-radius: 10px;
-          background-color: #d9d9d9;
           display: flex;
-          flex: 1;
           flex-direction: column;
           justify-content: center;
           align-items: center;
           height: 595px;
           margin-left: 90px;
-        }
-        
-        .${classPrefix}-image {
+          }
+
+          .${classPrefix}-video {
+          width: 100%;
+          height: 100%;
+          max-width: 595px;
+          max-height: 595px;
+          border-radius: 10px;
+          }
+
+          .${classPrefix}-image {
           aspect-ratio: 1;
           object-fit: contain;
           object-position: center;
@@ -117,9 +124,9 @@ const brandStory2Component = (editor: Editor) => {
           align-self: center;
           max-width: 100%;
           border-radius: 50%;
-        }
+          }
 
-       .${classPrefix}-title {
+          .${classPrefix}-title {
           text-align: center;
           font-family: Inter, sans-serif;
           font-size: 38px;
@@ -128,70 +135,69 @@ const brandStory2Component = (editor: Editor) => {
           line-height: normal;
           margin-top: 64px;
           margin-bottom: 0;
-        }
+          }
 
-        .${classPrefix}-description {
+          .${classPrefix}-description {
           text-align: center;
           padding: 0 84px;
           font-family: Inter, sans-serif;
           font-size: 18px;
           font-weight: 400;
-        }
+          }
 
-        @media (max-width: 770px) {
+          @media (max-width: 770px) {
           .${classPrefix}-section {
-            padding: 20px;
-            height: 693px;
-          }
-        
-          .${classPrefix}-panel {
-            display: none;
+          padding: 30px 20px;
           }
 
-          .${classPrefix}-image {
-            display: none;
-          }
-          .${classPrefix}-cardWrapper {
-              width: 100%;
-            }
-          .${classPrefix}-description {
-              padding: 30px;
-          }
-
-          .${classPrefix}-left-panel-info {
-              color: #000;
-              font-family: Inter;
-              font-size: 13px;
-              font-style: normal;
-              font-weight: 400;
-              line-height: 140%; /* 18.2px */
-              padding-top: 31px;
-          }
-
-          .${classPrefix}-left-panel-img {
-            display: block;
-            border-radius: 10px;
-            background-color: #d9d9d9;
-            display: flex;
-            flex: 1;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-            height: 595px;
-            margin-left: 90px;
+          .${classPrefix}-container {
+          flex-direction: column;
+          align-content: center;
+          justify-items: center;
+          max-width: none;
+          max-height: none;
           }
 
           .${classPrefix}-left-panel-heading {
-              color: #000;
-              text-align: center;
-              font-size: 26px;
-              font-style: normal;
-              font-weight: 600;
-              line-height: 35px; /* 134.615% */
-              margin-bottom: 45px;
+          color: #000;
+          text-align: center;
+          font-family: Inter;
+          font-size: 26px;
+          font-style: normal;
+          font-weight: 600;
+          line-height: 35px; /* 134.615% */
+          padding: 0 20px;
           }
-        }
-        
+
+          .${classPrefix}-left-panel-info {
+          color: #000;
+          font-family: Inter;
+          font-size: 13px;
+          font-style: normal;
+          font-weight: 400;
+          line-height: 140%; /* 18.2px */
+          text-align: center;
+          }
+
+          .${classPrefix}-right-container {
+          justify-items: center;
+          align-content: center;
+          height: auto;
+          margin: 0;
+          margin-top: 45px;
+          }
+
+          .${classPrefix}-video {
+          width: 100%;
+          height: 300px;
+          }
+          .${classPrefix}-cardWrapper {
+          width: 100%;
+          }
+          .${classPrefix}-description {
+          padding: 30px;
+          }
+          }
       `,
       },
     },
--- src/components/Editor/CustomBlocks/BrandConnect/Blocks/brandStory4.ts
+++ src/components/Editor/CustomBlocks/BrandConnect/Blocks/brandStory4.ts
@@ -31,90 +31,182 @@ const brandStory4Component = (editor: Editor) => {
         classes: [classPrefix],
         traits: [],
         components: `
-           <section class="${classPrefix}-section">
-              <div class="${classPrefix}-section-container">
-                <div class="${classPrefix}-section-header-prefix">
-                  Compelling header for
-                </div>
-                <div class="${classPrefix}-section-header">
-                  Your Brand Story
-                </div>
-                <div class="${classPrefix}-section-header-seperator"></div>
-                <div class="${classPrefix}-section-info">
-                Craft your brand story in a way that sounds authentic, evokes emotions in your users, and is consistent across channels. Tap into emotions such as joy, nostalgia, pain, etc, while narrating your journey.
-                Include the origin of the brand, its purpose & the values. Be very specific and simple with your brand message.
-                </div>
-              </div>
-           </section>
+          <section class="${classPrefix}-section">
+  <div class="${classPrefix}-container">
+    <div class="${classPrefix}-top-container">
+      <div class="${classPrefix}-imageWrapper">
+        <img loading="lazy"
+          srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/ca3d4956-b579-42e5-bfae-034be853a066?apiKey=d9eca8b0cf294176914b16d6a4d96de8&width=100 100w,
+                  https://cdn.builder.io/api/v1/image/assets/TEMP/ca3d4956-b579-42e5-bfae-034be853a066?apiKey=d9eca8b0cf294176914b16d6a4d96de8&width=200 200w,
+                  https://cdn.builder.io/api/v1/image/assets/TEMP/ca3d4956-b579-42e5-bfae-034be853a066?apiKey=d9eca8b0cf294176914b16d6a4d96de8&width=400 400w,
+                  https://cdn.builder.io/api/v1/image/assets/TEMP/ca3d4956-b579-42e5-bfae-034be853a066?apiKey=d9eca8b0cf294176914b16d6a4d96de8&width=800 800w,
+                  https://cdn.builder.io/api/v1/image/assets/TEMP/ca3d4956-b579-42e5-bfae-034be853a066?apiKey=d9eca8b0cf294176914b16d6a4d96de8&width=1200 1200w,
+                  https://cdn.builder.io/api/v1/image/assets/TEMP/ca3d4956-b579-42e5-bfae-034be853a066?apiKey=d9eca8b0cf294176914b16d6a4d96de8&width=1600 1600w, 
+                  https://cdn.builder.io/api/v1/image/assets/TEMP/ca3d4956-b579-42e5-bfae-034be853a066?apiKey=d9eca8b0cf294176914b16d6a4d96de8&width=2000 2000w, 
+                  https://cdn.builder.io/api/v1/image/assets/TEMP/ca3d4956-b579-42e5-bfae-034be853a066?apiKey=d9eca8b0cf294176914b16d6a4d96de8&"
+          class="${classPrefix}-image" />
+      </div>
+    </div>
+    <div class="${classPrefix}-bottom-container">
+      <span class="${classPrefix}-section-header-prefix">
+        Compelling header for
+      </span>
+      <span class="${classPrefix}-section-header">
+        Your Brand Story
+      </span>
+      <div class="${classPrefix}-section-header-seperator"></div>
+      <div class="${classPrefix}-section-info">
+        Craft your brand story in a way that sounds authentic, evokes emotions in your users, and is consistent across
+        channels. Tap into emotions such as joy, nostalgia, pain, etc, while narrating your journey.
+        Include the origin of the brand, its purpose & the values. Be very specific and simple with your brand message.
+      </div>
+      <div class="${classPrefix}-section-info">
+        Include the origin of the brand, its purpose & the values. Be very specific and simple with your brand message.
+      </div>
+    </div>
+  </div>
+</section>
         `,
         styles: `
         .${classPrefix}-section {
-          width: 100%;
-          height: 707px;
-          flex-shrink: 0;
-          background: #E9E9E9;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
+width: 100%;
+height: 707px;
+flex-shrink: 0;
+background: #e9e9e9;
+padding-top: 30px;
+margin: 0 auto auto auto;
+}
 
-        .${classPrefix}-section-container {
-          padding: 62px 72px 72px; 
-          background: #fff;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          width: 726px;
-          fill: #FFF;
-          filter: drop-shadow(0px 8px 24px rgba(0, 0, 0, 0.15));        
-          flex-direction: column;
-        }
+.${classPrefix}-container {
+display: flex;
+flex-direction: column;
+justify-items: center;
+align-content: center;
+align-items: center;
+justify-content: space-between;
+row-gap: 30px;
+}
 
-        .${classPrefix}-section-header-prefix {
-          color: #000;
-          text-align: center;
-          font-family: Inter;
-          font-size: 20px;
-          font-style: normal;
-          font-weight: 500;
-          line-height: normal;
-          margin-bottom: 19px;
-        }
+.${classPrefix}-image {
+aspect-ratio: 1;
+object-fit: contain;
+object-position: center;
+width: 90px;
+mix-blend-mode: multiply;
+overflow: hidden;
+align-self: center;
+max-width: 100%;
+border-radius: 50%;
+height: 90px;
+}
 
-        .${classPrefix}-section-header {
-          color: #000;
-          text-align: center;
-          font-family: Inter;
-          font-size: 40px;
-          font-style: normal;
-          font-weight: 600;
-          line-height: normal;
-        }
+.${classPrefix}-bottom-container {
+border-radius: 10px;
+padding: 72px;
+max-width: 726px;
+max-height: 416px;
+background: #fff;
+display: flex;
+align-items: center;
+flex-direction: column;
+justify-content: center;
+filter: drop-shadow(0px 8px 24px rgba(0, 0, 0, 0.15));
+}
 
-        .${classPrefix}-section-header-seperator {
-          width: 73px;
-          height: 4px;
-          flex-shrink: 0;
-          border-radius: 5px;
-          background: #D9D9D9;
-          margin-top: 22px;
-          margin-bottom: 29px;
-        }
+.${classPrefix}-section-header-prefix {
+color: #000;
+text-align: center;
+font-family: Inter;
+font-size: 20px;
+font-style: normal;
+font-weight: 500;
+line-height: normal;
+margin-bottom: 19px;
+}
 
-        .${classPrefix}-section-info {
+.${classPrefix}-section-header {
+color: #000;
+text-align: center;
+font-family: Inter;
+font-size: 40px;
+font-style: normal;
+font-weight: 600;
+line-height: normal;
+}
 
-          text-align: center;
-          font-family: Inter;
-          font-size: 15px;
-          font-style: normal;
-          font-weight: 400;
-          line-height: 140%; /* 21px */ 
-        }
+.${classPrefix}-section-header-seperator {
+width: 73px;
+height: 4px;
+flex-shrink: 0;
+border-radius: 5px;
+background: #d9d9d9;
+margin-top: 22px;
+margin-bottom: 29px;
+}
 
-        @media (max-width: 770px) {
-          .${classPrefix}-section {
-          }
-        }
+.${classPrefix}-section-info {
+text-align: center;
+font-family: Inter;
+font-size: 15px;
+font-style: normal;
+font-weight: 400;
+line-height: 140%; /* 21px */
+margin-bottom: 20px;
+}
+
+@media (max-width: 770px) {
+.${classPrefix}-section {
+height: 560px;
+width: 100%;
+max-width: 400px;
+max-height: 560px;
+padding: 50px 10px;
+}
+
+.${classPrefix}-bottom-container {
+padding: 60px 20px;
+max-width: 359px;
+max-height: 328px;
+}
+
+.${classPrefix}-section-header-prefix {
+color: #000;
+text-align: center;
+font-family: Inter;
+font-size: 16px;
+font-style: normal;
+font-weight: 500;
+line-height: 140%; /* 22.4px */
+margin: 0;
+}
+.${classPrefix}-section-header {
+color: #000;
+text-align: center;
+font-family: Inter;
+font-size: 26px;
+font-style: normal;
+font-weight: 600;
+line-height: normal;
+}
+.${classPrefix}-section-info {
+color: #000;
+text-align: center;
+font-family: Inter;
+font-size: 13px;
+font-style: normal;
+font-weight: 400;
+line-height: 140%; /* 18.2px */
+}
+.${classPrefix}-section-header-seperator {
+margin-top: 20px;
+margin-bottom: 20px;
+}
+
+.${classPrefix}-image {
+width: 47px;
+height: 47px;
+}
+}
       `,
       },
     },
