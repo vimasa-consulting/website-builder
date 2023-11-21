78d90ad16756db724e42b2bf5f4c4f39c94f2700
niranjanbala <niranjan.bala@gmail.com>
2023-11-21 13:06:45 +0530

    update block preview design

--- src/components/Editor/BlockPreviewPopup.tsx
+++ src/components/Editor/BlockPreviewPopup.tsx
@@ -16,17 +16,32 @@ const BlockPreviewPopup: React.FC<BlockPreviewPopupProps> = ({ grapeJSEditor }) 
     }
     return (
         <div id="customModalPreviewPopup" className="preview-modal">     
-            <div className="gjs-row">
-                <div className="gjs-cell">
-                    <div >Hi Adharsh</div>
+            <div className="container">
+                <p className="f25">Hi Adarsh!</p>
+                <p className="f30">our experts have built your page for maximum pursuassion</p>
+                <p className="f60">It's built with <span className="color-1">20% Trsut,</span><span  className="color-2">50% Logic,</span></p>
+                <p className="f60"><span  className="color-3">25% Brand Connect,</span><span  className="color-4">5% FOMO</span>&<span  className="color-5">10% value</span></p>
+            </div>
+            <div className="filter-container">
+                <div className="filter-section">
+                    <div className="left">
+                        <p className="pill  bg-1">Above the Fold</p>
+                        <p className="data"> this block helps you create powerful first impression by bringing together all the strengths of your prosuct and brands in one place</p>
+                    </div>
+                    <div className="right">
+                        <img src="https://development.d13nogs6jpk1jf.amplifyapp.com/editor/blocks/Za1.png" />
+                    </div>
+                </div>
+                <div className="filter-section">
+                    <div className="left">
+                        <p className="pill bg-2">Above the Fold</p>
+                        <p className="data"> this block helps you create powerful first impression by bringing together all the strengths of your prosuct and brands in one place</p>
+                    </div>
+                    <div className="right">
+                        <img src="https://development.d13nogs6jpk1jf.amplifyapp.com/editor/blocks/Za1.png" />
+                    </div>
                 </div>
             </div>
-            <div className="gjs-row">
-                <div className="gjs-cell">
-                <div>Our experts have built your page for maximum persuasion.
-                </div>
-                </div>
-            </div>            
         </div>
     );
 };
