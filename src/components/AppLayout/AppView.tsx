bb5d33441ff8030074c25aa5ed7638be86eb7ed9
Ashish Rao D S <ashishraods@SMGCC-MAC014.local>
2023-11-30 12:12:32 +0530

    fix: Matching getting started section with figma

--- src/components/AppLayout/AppView.tsx
+++ src/components/AppLayout/AppView.tsx
@@ -2,6 +2,6 @@ import React from "react"
 
 export default function AppView({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex-grow px-12 py-6 border-l border-0.1 border-rgb-77-77-77">{children}</div>
+    <div className="flex-grow px-12 pl-[100px] py-6 border-l border-0.1 border-rgb-77-77-77">{children}</div>
   );
 }
