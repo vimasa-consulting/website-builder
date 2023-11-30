35239836c3b81edcc43a5e9eeb54c6b3afd9743c
Ashish Rao D S <ashishraods@SMGCC-MAC014.local>
2023-11-30 15:54:46 +0530

    fix: type issue

--- src/components/Editor/Popover/PopoverConfirm.tsx
+++ src/components/Editor/Popover/PopoverConfirm.tsx
@@ -9,23 +9,24 @@ export interface PopoverConfirmProps extends PopoverProps {
     onConfirm?: () => void;
     onCancel?: () => void;
     text?: React.ReactNode;
+    children?: React.ReactNode; 
 }
 
 export default function PopoverConfirm({ children, text, onConfirm, onCancel, className = 'w-[200px]', ...props }: PopoverConfirmProps) {
     const i18nStore = useI18nStore();
     return (
-        <Popover {...props} overlay title={false} buttonAs="span" /*handler={children}*/>
+        <Popover {...props} overlay title={false} buttonAs="span" handler={children}>
             {({ close }) => (
                 <Grid className={className} space="m" col>
                     <GridItem grow>
-                        { text || i18nStore.t('confirmAction') }
+                        {text || i18nStore.t('confirmAction')}
                     </GridItem>
                     <Grid justify="end" items="center" space="m">
                         <Button variant="pr" size="m2" onClick={() => { close(); onConfirm?.() }}>
-                            { i18nStore.t('confirm') }
+                            {i18nStore.t('confirm')}
                         </Button>
                         <Button size="m2" onClick={() => { close(); onCancel?.() }}>
-                            { i18nStore.t('cancel') }
+                            {i18nStore.t('cancel')}
                         </Button>
                     </Grid>
                 </Grid>
--- src/components/Editor/Popover/index.tsx
+++ src/components/Editor/Popover/index.tsx
@@ -8,93 +8,92 @@ import { calculatePosition, PosOptions } from '../Tooltip';
 import { useClickOutside } from '../../utils/hooks';
 
 export type RenderProps = {
-   close(): void,
+  close(): void,
 };
 
 export interface PopoverProps {
-    handler?: React.ReactNode,
-    children?: React.ReactNode | ((props: RenderProps) => void),
-    title?: React.ReactNode,
-    className?: string,
-    static?: boolean,
-    clickAway?: boolean,
-    fixed?: boolean,
-    padding?: boolean,
-    overlay?: boolean,
-    pos?: PosOptions,
-    canOpen?: () => boolean,
-    open?: boolean,
-    onClose?: () => void,
-    size?: 's' | 'sm' | 'md',
-    topIcon?: boolean,
-    buttonAs?: ElementType<any>,
+  handler?: React.ReactNode,
+  children?: React.ReactNode | ((props: RenderProps) => React.ReactNode),
+  title?: React.ReactNode,
+  className?: string,
+  static?: boolean,
+  clickAway?: boolean,
+  fixed?: boolean,
+  padding?: boolean,
+  overlay?: boolean,
+  pos?: PosOptions,
+  canOpen?: () => boolean,
+  open?: boolean,
+  onClose?: () => void,
+  size?: 's' | 'sm' | 'md',
+  topIcon?: boolean,
+  buttonAs?: ElementType<any>,
 };
 
 export default function Popover({ children, handler, title, fixed, size, padding = true, overlay, clickAway, topIcon, pos, open: isOpen, onClose, buttonAs, className, ...props }: PopoverProps) {
-    const refBtn = useRef(null);
-    const refPanel = useRef(null);
-    const [open, setOpen] = useState(!!isOpen);
-    const [posit, setPos] = useState({ top: 0, left: 0 });
+  const refBtn = useRef(null);
+  const refPanel = useRef(null);
+  const [open, setOpen] = useState(!!isOpen);
+  const [posit, setPos] = useState({ top: 0, left: 0 });
 
-    const close = () => {
-      setOpen(false);
-      onClose?.();
-    };
+  const close = () => {
+    setOpen(false);
+    onClose?.();
+  };
 
-    useClickOutside(refPanel, {
-      onTrigger: close,
-      enabled: clickAway && open,
-    });
+  useClickOutside(refPanel, {
+    onTrigger: close,
+    enabled: clickAway && open,
+  });
 
-    useEffect(() => {
-      setOpen(!!isOpen);
-    }, [isOpen]);
+  useEffect(() => {
+    setOpen(!!isOpen);
+  }, [isOpen]);
 
-    useEffect(() => {
-      const refBtnEl = refBtn.current;
-      const refPanelEl = refPanel.current;
-      if (refBtnEl && refPanelEl) {
-        if (open) {
-          const updatePos = () => setPos(calculatePosition(refBtnEl, refPanelEl, pos));
-          setTimeout(updatePos);
-        }
+  useEffect(() => {
+    const refBtnEl = refBtn.current;
+    const refPanelEl = refPanel.current;
+    if (refBtnEl && refPanelEl) {
+      if (open) {
+        const updatePos = () => setPos(calculatePosition(refBtnEl, refPanelEl, pos));
+        setTimeout(updatePos);
       }
-    }, [refBtn, refPanel, open]); // eslint-disable-line
-    /*
-    {
-                  !open ? null : (typeof children === 'function' ? children({ close }) : children)
-                }
-    */
-    return (
-      <PO className={cx('popover relative flex', className)}>
-        <PO.Button ref={refBtn} as={buttonAs} className="w-full">
-          <div onClick={() => !isDef(isOpen) && setOpen(!open)}>{ handler }</div>
-        </PO.Button>
-        { open && overlay && <div onClick={close} className="z-10 fixed inset-0"/> }
-        <Transition
-              as={Fragment}
-              show={open}
-              unmount={false}
-              enter="transition ease-out duration-200" enterFrom="opacity-0 translate-y-1" enterTo="opacity-100"
-              leave="transition ease-in duration-150" leaveFrom="opacity-100" leaveTo="opacity-0 translate-y-1"
-            >
-            <PO.Panel
-              className={cx([
-                'fixed z-20',
-                topIcon && 'mt-5',
-                size === 's' && 'w-full max-w-[240px]',
-                size === 'sm' && 'w-full max-w-sm',
-                size === 'md' && 'w-full max-w-md',
-              ])}
-              onClick={stop}
-              style={posit}
-              static
-            >              
-              <Card ref={refPanel} title={title} onClose={close} padding={padding}>
-                
-              </Card>
-            </PO.Panel>
-        </Transition>
+    }
+  }, [refBtn, refPanel, open]); // eslint-disable-line
+
+
+  return (
+    <PO className={cx('popover relative flex', className)}>
+      <PO.Button ref={refBtn} as={buttonAs} className="w-full">
+        <div onClick={() => !isDef(isOpen) && setOpen(!open)}>{handler}</div>
+      </PO.Button>
+      {open && overlay && <div onClick={close} className="z-10 fixed inset-0" />}
+      <Transition
+        as={Fragment}
+        show={open}
+        unmount={false}
+        enter="transition ease-out duration-200" enterFrom="opacity-0 translate-y-1" enterTo="opacity-100"
+        leave="transition ease-in duration-150" leaveFrom="opacity-100" leaveTo="opacity-0 translate-y-1"
+      >
+        <PO.Panel
+          className={cx([
+            'fixed z-20',
+            topIcon && 'mt-5',
+            size === 's' && 'w-full max-w-[240px]',
+            size === 'sm' && 'w-full max-w-sm',
+            size === 'md' && 'w-full max-w-md',
+          ])}
+          onClick={stop}
+          style={posit}
+          static
+        >
+          <Card ref={refPanel} title={title} onClose={close} padding={padding}>
+            {
+              !open ? null : (typeof children === 'function' ? children({ close }) : children)
+            }
+          </Card>
+        </PO.Panel>
+      </Transition>
     </PO>
-    );
-  }
+  );
+}
