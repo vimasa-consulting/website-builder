028aa16349cdb0d8e8434b15174e7f666ec3d44c
niranjanbala <niranjan.bala@gmail.com>
2023-12-17 11:32:15 +0530

    heatmap routes change

--- src/app/(authenticated-routes)/analytics/page.tsx
+++ src/app/(authenticated-routes)/analytics/page.tsx
@@ -6,26 +6,26 @@ export default function Page() {
         <iframe 
           width="100%" 
           height="350" 
-          src="https://ec2-3-218-241-8.compute-1.amazonaws.com/index.php?module=Widgetize&action=iframe&disableLink=1&widget=1&moduleToWidgetize=Live&actionToWidgetize=getSimpleLastVisitCount&idSite=19&period=day&date=yesterday"
+          src="https://development.d13nogs6jpk1jf.amplifyapp.com/matomo/?module=Widgetize&action=iframe&disableLink=1&widget=1&moduleToWidgetize=Live&actionToWidgetize=getSimpleLastVisitCount&idSite=11&period=day&date=yesterday"
         />
       <p>Visitors Over time </p>
         <iframe 
           width="100%" 
           height="350" 
-          src="https://ec2-3-218-241-8.compute-1.amazonaws.com/index.php?forceView=1&viewDataTable=graphEvolution&module=Widgetize&action=iframe&disableLink=1&widget=1&moduleToWidgetize=VisitsSummary&actionToWidgetize=getEvolutionGraph&idSite=19&period=day&date=yesterday"
+          src="https://development.d13nogs6jpk1jf.amplifyapp.com/matomo/?forceView=1&viewDataTable=graphEvolution&module=Widgetize&action=iframe&disableLink=1&widget=1&moduleToWidgetize=VisitsSummary&actionToWidgetize=getEvolutionGraph&idSite=11&period=day&date=yesterday"
         />
 
         <p>Visitors Overview </p>
         <iframe 
           width="100%" 
           height="350" 
-          src="https://ec2-3-218-241-8.compute-1.amazonaws.com/index.php?module=Widgetize&action=iframe&containerId=VisitOverviewWithGraph&disableLink=1&widget=1&moduleToWidgetize=CoreHome&actionToWidgetize=renderWidgetContainer&idSite=19&period=day&date=yesterday"
+          src="https://development.d13nogs6jpk1jf.amplifyapp.com/matomo/?module=Widgetize&action=iframe&containerId=VisitOverviewWithGraph&disableLink=1&widget=1&moduleToWidgetize=CoreHome&actionToWidgetize=renderWidgetContainer&idSite=11&period=day&date=yesterday"
         />
         <p>Visitors Map </p>
         <iframe 
           width="100%" 
           height="350" 
-          src="https://ec2-3-218-241-8.compute-1.amazonaws.com/index.php?module=Widgetize&action=iframe&disableLink=1&widget=1&moduleToWidgetize=UserCountryMap&actionToWidgetize=visitorMap&idSite=19&period=day&date=yesterday"
+          src="https://development.d13nogs6jpk1jf.amplifyapp.com/matomo/?module=Widgetize&action=iframe&disableLink=1&widget=1&moduleToWidgetize=UserCountryMap&actionToWidgetize=visitorMap&idSite=11&period=day&date=yesterday"
         />
     </div>    
     </div>
