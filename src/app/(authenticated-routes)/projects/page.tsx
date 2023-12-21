f06949ed89d233595d7d3f1fec9deb8ec767665f
niranjanbala <niranjan.bala@gmail.com>
2023-12-21 16:54:31 +0530

    files

--- src/app/(authenticated-routes)/projects/page.tsx
+++ src/app/(authenticated-routes)/projects/page.tsx
@@ -35,6 +35,7 @@ export default function Page() {
         name: payload.inputOneData,
         projectHostingAlias: payload.inputTwoData,
         collaborators: [],
+        files:[],
         organizationId: cachedUser?.organizations[0] || '',
       }
 
