725760b342266cd6a067944646c8e25b7d6cf20b
niranjanbala <niranjan.bala@gmail.com>
2023-12-22 16:09:51 +0530

    click tracking

--- src/app/(authenticated-routes)/analytics/page.tsx
+++ src/app/(authenticated-routes)/analytics/page.tsx
@@ -27,6 +27,15 @@ export default function Page() {
           height="350" 
           src="https://development.d13nogs6jpk1jf.amplifyapp.com/matomo/?module=Widgetize&action=iframe&disableLink=1&widget=1&moduleToWidgetize=UserCountryMap&actionToWidgetize=visitorMap&idSite=11&period=day&date=yesterday"
         />
+        <p>Click Tracking </p>
+        <div id="widgetIframe">
+        <iframe 
+            width="100%" 
+            height="350" 
+            src="https://development.d13nogs6jpk1jf.amplifyapp.com/matomo/?module=Widgetize&action=iframe&secondaryDimension=eventAction&disableLink=1&widget=1&moduleToWidgetize=Events&actionToWidgetize=getCategory&idSite=31&period=year&date=2023-12-20" 
+            />
+                  
+        </div>
     </div>    
     </div>
   );
