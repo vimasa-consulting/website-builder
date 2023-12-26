0a1b0112588f6865a16132dc2299c6e5c2da0185
niranjanbala <niranjan.bala@gmail.com>
2023-12-26 13:26:27 +0530

    hiding some more components

--- src/components/Editor/BlockManager/index.tsx
+++ src/components/Editor/BlockManager/index.tsx
@@ -27,7 +27,7 @@ export default observer(function BlockManager() {
         if (searchValue &&  !includesSearch(ctg, searchValue) && !includesSearch(blockLabel(item), searchValue)) {
             return res;
         }
-        const skipComponent=['map','textarea','input','select','label','checkbox','radio','form'].includes(item.id);
+        const skipComponent=['column2','column1','column3','column3-7','map','textarea','input','select','label','checkbox','radio','form'].includes(item.id);
         if(!skipCat){
             console.log(item);
         }        
