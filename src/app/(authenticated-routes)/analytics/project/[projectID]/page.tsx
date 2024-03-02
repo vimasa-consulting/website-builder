d3a1f03963a36ccc67965892db6b2906abe5d318
niranjanbala <niranjan.bala@coextrix.com>
2024-03-02 13:20:07 +0530

    Update page.tsx

--- src/app/(authenticated-routes)/analytics/project/[projectID]/page.tsx
+++ src/app/(authenticated-routes)/analytics/project/[projectID]/page.tsx
@@ -1,11 +1,11 @@
 export default function Page({ params }: { params: { projectID: string } }) {
 
   const idSite=params.projectID; // TODO: update from project data;
-  const linkRealTime=`https://development.d13nogs6jpk1jf.amplifyapp.com/matomo/?module=Widgetize&action=iframe&disableLink=1&widget=1&moduleToWidgetize=Live&actionToWidgetize=getSimpleLastVisitCount&idSite=${idSite}&period=day&date=yesterday`;
-  const linkVisitorsOverTime=`https://development.d13nogs6jpk1jf.amplifyapp.com/matomo/?forceView=1&viewDataTable=graphEvolution&module=Widgetize&action=iframe&disableLink=1&widget=1&moduleToWidgetize=VisitsSummary&actionToWidgetize=getEvolutionGraph&idSite=${idSite}&period=day&date=yesterday`;
-  const linkVisitorsOverview=`https://development.d13nogs6jpk1jf.amplifyapp.com/matomo/?module=Widgetize&action=iframe&containerId=VisitOverviewWithGraph&disableLink=1&widget=1&moduleToWidgetize=CoreHome&actionToWidgetize=renderWidgetContainer&idSite=${idSite}&period=day&date=yesterday`;
-  const linkVisitorsMap=`https://development.d13nogs6jpk1jf.amplifyapp.com/matomo/?module=Widgetize&action=iframe&disableLink=1&widget=1&moduleToWidgetize=UserCountryMap&actionToWidgetize=visitorMap&idSite=${idSite}&period=day&date=yesterday`;
-  const linkClickTracking=`https://development.d13nogs6jpk1jf.amplifyapp.com/matomo/?module=Widgetize&action=iframe&secondaryDimension=eventAction&disableLink=1&widget=1&moduleToWidgetize=Events&actionToWidgetize=getCategory&idSite=${idSite}&period=year&date=yesterday0`;
+  const linkRealTime=`https://ec2-54-209-127-49.compute-1.amazonaws.com/index.php?module=Widgetize&action=iframe&disableLink=1&widget=1&moduleToWidgetize=Live&actionToWidgetize=getSimpleLastVisitCount&idSite=${idSite}&period=day&date=yesterday`;
+  const linkVisitorsOverTime=`https://ec2-54-209-127-49.compute-1.amazonaws.com/index.php?forceView=1&viewDataTable=graphEvolution&module=Widgetize&action=iframe&disableLink=1&widget=1&moduleToWidgetize=VisitsSummary&actionToWidgetize=getEvolutionGraph&idSite=${idSite}&period=day&date=yesterday`;
+  const linkVisitorsOverview=`https://ec2-54-209-127-49.compute-1.amazonaws.com/index.phpmodule=Widgetize&action=iframe&containerId=VisitOverviewWithGraph&disableLink=1&widget=1&moduleToWidgetize=CoreHome&actionToWidgetize=renderWidgetContainer&idSite=${idSite}&period=day&date=yesterday`;
+  const linkVisitorsMap=`https://ec2-54-209-127-49.compute-1.amazonaws.com/index.phpmodule=Widgetize&action=iframe&disableLink=1&widget=1&moduleToWidgetize=UserCountryMap&actionToWidgetize=visitorMap&idSite=${idSite}&period=day&date=yesterday`;
+  const linkClickTracking=`https://ec2-54-209-127-49.compute-1.amazonaws.com/index.php?module=Widgetize&action=iframe&secondaryDimension=eventAction&disableLink=1&widget=1&moduleToWidgetize=Events&actionToWidgetize=getCategory&idSite=${idSite}&period=year&date=yesterday0`;
   return (
     <div>
       <div id="widgetIframeRTVC">
